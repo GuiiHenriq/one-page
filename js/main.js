d85563d9945ec1dc11488e404cb4d97126d83dd5
Guilherme Henrique <guilhermeerba@hotmail.com>
2018-11-20 11:59:58 -0200

    Primeiro Commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// Scroll Suave Link Interno
+const linkInterno = document.querySelectorAll('.js-menu a[href^="#"]');
+
+function scrollToSection(event) {
+  event.preventDefault();
+  const href = event.currentTarget.getAttribute('href');
+  const section = document.querySelector(href);
+
+  section.scrollIntoView({
+    behavior: 'smooth',
+    block: 'start',
+  });
+}
+
+linkInterno.forEach((link) => {
+  link.addEventListener('click', scrollToSection);
+});
