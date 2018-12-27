2cbe70e2775c5098e6856bafba460524d3c1833e
Guilherme Henrique <guilhermeerba@hotmail.com>
2018-12-27 16:48:46 -0200

    Versão Final

--- js/main.js
+++ js/main.js
@@ -15,3 +15,20 @@ function scrollToSection(event) {
 linkInterno.forEach((link) => {
   link.addEventListener('click', scrollToSection);
 });
+
+
+// Menu Mobile
+const buttonOpen = document.querySelector('.btn-open');
+const buttonClose = document.querySelector('.btn-close');
+const menu = document.querySelector('.header-menu ul');
+
+function openMenu() {
+  menu.classList.toggle('ativo');
+}
+
+function closeMenu() {
+  menu.classList.toggle('ativo');
+}
+
+buttonOpen.addEventListener('click', openMenu);
+buttonClose.addEventListener('click', closeMenu);
